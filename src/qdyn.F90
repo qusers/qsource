1deadb941e31be1746de47e23d1ccf76bbf5651b
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2015-02-20 14:10:17 +0100

    Revert "Creating development hierarchy."
    This reverts commit 4381d2d5e50dcbb0557398ca0c2d4559c009358f.

--- src/qdyn.F90
+++ src/qdyn.F90
@@ -1,188 +1,205 @@
-! (c) 2015 uppsala molekylmekaniska hb, uppsala, sweden
-! qdyn.f90
-! initial date: 2000
-! by johan Ã¥qvist, john marelius, anders kaplan & martin nervall
-! qdyn molecular dynamics main program
+!	(C) 2000 Uppsala Molekylmekaniska HB, Uppsala, Sweden
 
-program qdyn
-  use md
-  use mpiglob ! use mpi global data
+!	qdyn.f90
+!	by Johan Åqvist, John Marelius, Anders Kaplan & Martin Nervall
+
+
+!	Qdyn molecular dynamics main program
+
+program Qdyn5
+  use MD								
+  use MPIGLOB ! use MPI global data
+#if defined (_DF_VERSION_)
+  use dfport							!	portability lib for signals, used by Compaq Visual Fortran compiler
+#endif
 
   implicit none
-  ! version data
-  character(10) :: qdyn_version = '5.7'
-  character(12) :: qdyn_date = '2015-02-08'
-#if defined (use_mpi)
-  character(10) :: qdyn_suffix = '_parallel'
-#else
-  character(10) :: qdyn_suffix = ''
-#endif
-  
-#if defined (use_mpi)
-  ! mpi error code
-  integer :: qdyn_ierr
-#endif
-  
-  ! signal handler data and declarations
-  integer(4) :: sigret
-  integer(4), parameter :: sigint = 2 ! ctrl-c signal
-  integer(4), parameter :: sigkill = 9 ! kill/ctrl-break signal
-  integer(4), parameter :: sigabrt = 6 ! kill/ctrl-break signal
 
+  ! version data
+  character(10)					:: QDYN_VERSION = '5.06'
+  character(12)					:: QDYN_DATE = '2007-06-27'
+#if defined (USE_MPI)
+  character(10)					:: QDYN_SUFFIX = '_parallel'
+#else
+  character(10)					:: QDYN_SUFFIX = ''
+#endif
+
+
+#if defined (USE_MPI)
+  ! MPI error code
+  integer						:: qdyn_ierr
+#endif
+
+  ! signal handler data and declarations
+  integer(4)					:: sigret
+#if defined (_DF_VERSION_)
+  ! nothing
+#else
+  integer(4), parameter			:: SIGINT = 2	! CTRL-C signal
+  integer(4), parameter			:: SIGKILL = 9	! kill/CTRL-BREAK signal
+  integer(4), parameter			:: SIGABRT = 6	! kill/CTRL-BREAK signal
+#if ! defined(__PATHCC__)
+  interface
+	integer(4) function signal( signum, proc, flag )
+	  integer(4) signum, flag
+	  external proc
+	end function
+  end interface
+#endif
+#endif
   external sigint_handler
   external sigkill_handler
   external sigabrt_handler
 
-#if defined (use_mpi)
-  ! initialize mpi
-  call mpi_init(qdyn_ierr)
-  if (qdyn_ierr .ne. mpi_success) call die('failure at mpi init')
-  call mpi_comm_rank(mpi_comm_world, nodeid, qdyn_ierr)
-  call mpi_comm_size(mpi_comm_world, numnodes, qdyn_ierr)
+#if defined (USE_MPI)
+  ! initialize MPI
+  call MPI_Init(qdyn_ierr)
+  if (qdyn_ierr .ne. MPI_SUCCESS) call die('failure at MPI init')
+  call MPI_Comm_rank(MPI_COMM_WORLD, nodeid, qdyn_ierr)
+  call MPI_Comm_size(MPI_COMM_WORLD, numnodes, qdyn_ierr)
 #else
-  nodeid = 0
-  numnodes = 1
+	nodeid = 0
+	numnodes = 1
 #endif
-  
-  ! init signal handlers
-  sigret = signal(sigint, sigint_handler, -1_4)
-  sigret = signal(sigkill, sigkill_handler, -1_4)
-  sigret = signal(sigabrt, sigabrt_handler, -1_4)
 
-  ! initialize static data, display banner etc
+  ! init signal handlers
+  sigret = SIGNAL(SIGINT, sigint_handler, -1_4)
+  sigret = SIGNAL(SIGKILL, sigkill_handler, -1_4)
+!  sigret = SIGNAL(SIGABRT, sigabrt_handler, -1_4)
+
+  ! initialise static data, display banner etc
   call startup
 
   if (nodeid .eq. 0) then
-    ! master node: read input and initialize
-    if (.not.initialize()) call die('invalid data in input file') ! read input data
-    call open_files ! open necessary files
-    call topology ! read topology
-    call prep_coord ! read coords, solvates etc
-    if (nstates > 0) call get_fep ! read fep/evb strategy
-    !remove things with code 0 and maybe excluded bonded interactions
-    call prep_sim ! prepare for simulation (calc. inv. mass, total charge,...)
-    call close_input_files ! close input files
-    call init_shake
-    call make_nbqqlist
-    call shrink_topology
-    call nbmonitorlist
-    call init_trj
+	! master node: read input and initialise
+	if(.not. initialize()) call die('Invalid data in input file')						! read input data
+	call open_files						! open necessary files
+	call topology				! read topology
+	call prep_coord						! read coords, solvates etc
+	if ( nstates > 0 ) call get_fep	! read fep/evb strategy
+	!remove things with code 0 and maybe excluded bonded interactions
 
-    ! generate maxwellian velocities and shake initial x and v if necessary
-    if (iseed > 0) then
+	call prep_sim						! prepare for simulation (calc. inv. mass, total charge,...)
+	call close_input_files				! close input files
+
+	call init_shake
+	call make_nbqqlist
+	call shrink_topology
+	call nbmonitorlist
+	call init_trj
+
+    ! generate Maxwellian velocities and shake initial x and v if necessary
+    if ( iseed > 0 ) then
       call maxwell
-      call initial_shaking
-      call stop_cm_translation
+	  call initial_shaking
+ 	  call stop_cm_translation
     end if
   end if
 
-#if defined (use_mpi)
-  ! initialize slave nodes
+#if defined (USE_MPI)
+  ! initialise slave nodes
   if (numnodes .gt. 1) call init_nodes
 #endif
-  
-  ! count non-bonded pairs to get the maximum number, then distribute them among the nodes
-  call distribute_nonbonds
+	! count non-bonded pairs to get the maximum number, then distribute them among the nodes
 
-  ! do the work!
+call distribute_nonbonds
+
+ ! do the work!
   call md_run
 
   if (nodeid .eq. 0) then
-    ! master node: close output files
-    call close_output_files
+	! master node: close output files
+	call close_output_files
   end if
 
   ! deallocate memory etc.
   call shutdown
 
-#if defined (use_mpi)
-  ! shut down mpi
-  call mpi_finalize(qdyn_ierr)
+#if defined (USE_MPI)
+  ! shut down MPI
+  call MPI_Finalize(qdyn_ierr)
 #endif
-  
+
 contains
 
-  !-------------------------------------------------------------------------------
-  ! startup code
-  !-------------------------------------------------------------------------------
-  subroutine startup
-    integer :: i
-    integer :: datum(8)
+!-----------------------------------------------------------------------
 
-    if (nodeid .eq. 0) then
-      ! start-of-header
-      write (*, '(79a)') ('#', i = 1, 79)
-#if defined (dum)
-      write(*, '(a,a,a)') 'qdum input checker version ', trim(qdyn_version), ' initializing'
-#elif defined(eval)
-      write(*, '(a,a,a)') 'qdyn evaluation version ', trim(qdyn_version), ' initializing'
-      write(*, '(a)') 'this version is for evaluation purposes only.'
-      write(*, '(a)') 'optimisations are disabled - runs at <20% of maximum speed.'
+! startup/shutdown code
+
+subroutine startup
+  integer						::	i
+  integer                       :: datum(8)
+
+  if (nodeid .eq. 0) then
+	! start-of-header
+	write (*,'(79a)') ('#',i=1,79)
+#if defined (DUM)
+	write(*,'(a,a,a)') 'QDum input checker version ', trim(QDYN_VERSION), ' initialising'
+#elif defined(EVAL)
+	write(*,'(a,a,a)') 'QDyn evaluation version ', trim(QDYN_VERSION), ' initialising'
+	write(*,'(a)') 'This version is for evaluation purposes only.'
+	write(*,'(a)') 'Optimisations are disabled - runs at <20% of maximum speed.'
 #else
-      write(*, '(a,a,a,a)') 'qdyn version ', trim(qdyn_version), trim(qdyn_suffix), ' initializing'
+	write(*,'(a,a,a,a)') 'QDyn version ', trim(QDYN_VERSION), trim(QDYN_SUFFIX),' initialising'
 #endif
-      call date_and_time(values = datum)
-      write(*, 130) datum(1), datum(2), datum(3), datum(5), datum(6), datum(7)
-      130 format('current date ', i4, '-', i2, '-', i2, ' and time ', i2, ':', i2, ':', i2)
-    end if
+	call date_and_time(values=datum)
+	write(*,130) datum(1),datum(2),datum(3),datum(5),datum(6),datum(7)  
+130	format('Current date ',i4,'-',i2,'-',i2,' and time ',i2,':',i2,':',i2)
+  end if
 
 
-    ! initialize used modules
-    call md_startup
+  ! initialise used modules
+  call md_startup
 
-  end subroutine startup
+end subroutine startup
 
-  !-------------------------------------------------------------------------------
-  ! shutdown code
-  !-------------------------------------------------------------------------------
-  subroutine shutdown
-    integer :: i
+!-----------------------------------------------------------------------
 
-    if (nodeid .eq. 0) then
-#if defined (dum)
-      write(*, *) 'qdum input checker version ', qdyn_version, ' terminated normally.'
+subroutine shutdown
+  integer						:: i
+
+  if (nodeid .eq. 0) then
+#if defined (DUM)
+	write(*,*) 'QDum input checker version ', QDYN_VERSION, ' terminated normally.'
 #else
-      write(*, *) 'qdyn version ', trim(qdyn_version), trim(qdyn_suffix), ' terminated normally.'
+	write(*,*) 'QDyn version ', trim(QDYN_VERSION), trim(QDYN_SUFFIX), ' terminated normally.'
 #endif
-      write (*, '(79a)') ('#', i = 1, 79)
-    end if
+	write (*,'(79a)') ('#',i=1,79)
+  end if
 
-    ! call md's shutdown
-    call md_shutdown
-  end subroutine shutdown
+  ! call md's shutdown
+  call md_shutdown
+end subroutine shutdown
 
-  !-----------------------------------------------------------------------
-  !interface
-  integer(4) function signal(signum, proc, flag)
-    integer(4) signum, flag
-    external proc
-  end function
-  !end interface
+!-----------------------------------------------------------------------
 
-end program qdyn
+end program Qdyn5
 
 ! signal handlers
 
-integer(4) function sigint_handler(sig_num)
-  use md
+INTEGER(4) FUNCTION sigint_handler(sig_num)
+  use MD
   implicit none
-  integer(4) :: sig_num
-  call die('user request (control-c)')
-  sigint_handler = 1
-end function sigint_handler
+  INTEGER(4)					:: sig_num
 
-integer(4) function sigkill_handler(sig_num)
-  use md
+  call die('user request (control-C)')
+  sigint_handler = 1
+END FUNCTION sigint_handler
+
+INTEGER(4) FUNCTION sigkill_handler(sig_num)
+  use MD
   implicit none
-  integer(4) :: sig_num
+  INTEGER(4)					:: sig_num
+
   call die('kill signal')
   sigkill_handler = 1
-end function sigkill_handler
+END FUNCTION sigkill_handler
 
-integer(4) function sigabrt_handler(sig_num)
-  use md
+INTEGER(4) FUNCTION sigabrt_handler(sig_num)
+  use MD
   implicit none
-  integer(4) :: sig_num
+  INTEGER(4)					:: sig_num
+
   call die('kill signal')
   sigabrt_handler = 1
-end function sigabrt_handler
+END FUNCTION sigabrt_handler
