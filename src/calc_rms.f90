5699eb8a5d501ec7bd8ca91e969638c69827c516
Florian van der Ent <florianvdent@gmail.com>
2022-08-25 18:04:44 +0200

    Added license header

--- src/calc_rms.f90
+++ src/calc_rms.f90
@@ -1,4 +1,21 @@
-!------------------------------------------------------------------------------!
+! Q version 5.6
+! 
+! Copyright © 2014 Johan Åqvist, John Marelius
+! 
+! This program is free software; you can redistribute it and/or modify it under the 
+! terms of the GNU General Public License as published by the Free 
+! Software Foundation; either version 2 of the License, or any later version.
+! 
+! This program is distributed in the hope that it will be useful, 
+! but WITHOUT ANY WARRANTY; without even the implied warranty of 
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+! See the GNU General Public License for more details.
+! 
+! You should have received a copy of the GNU General Public License along with 
+! this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
+! Street, Fifth Floor, Boston, MA  02110-1301, USA. Also add information on 
+! how to contact you by electronic and paper mail.
+!
 !  (C) 2000 Uppsala Molekylmekaniska HB, Uppsala, Sweden
 !  calc_rms.f90
 !  by John Marelius
